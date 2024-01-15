4359220de017bd38432218cc2a519b144b9b7d44
incub4t0r <dchung0812@gmail.com>
2024-01-15 10:40:33 -0500

    initial commit

---
+++ RNGMines/Patches/MinePatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace RNGMines.Patches
+{
+    internal class MinePatch
+    {
+        private static bool HasLineOfSight(Vector3 source, Vector3 target, LayerMask obstructionLayer, int distanceRange = 30)
+        {
+            Vector3 direction = target - source;
+            float distance = direction.magnitude;
+
+            if (distance > distanceRange)
+            {
+                return false;
+            }
+
+            Ray ray = new Ray(source, direction.normalized);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, distance, obstructionLayer))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(Landmine), "Update")]
+        [HarmonyPostfix]
+        private static void UpdatePatch(Landmine __instance)
+        {
+            PlayerControllerB player = GameNetworkManager.Instance.localPlayerController.GetComponentInChildren<PlayerControllerB>();
+
+            if (player == null)
+            {
+                return;
+            }
+
+            if (__instance.hasExploded)
+            {
+                return;
+            }
+
+            bool playerSeesMine = HasLineOfSight(player.transform.position, __instance.transform.position, StartOfRound.Instance.collidersAndRoomMaskAndDefault, 15);
+
+            if (playerSeesMine)
+            {
+                int rnjesus = UnityEngine.Random.Range(0, 10000);
+                if (rnjesus == 666)
+                {
+                    RNGMineBase.GetLogger().LogInfo("RNJesus has forsaken you, triggering mine");
+                    __instance.GetType().GetMethod("TriggerMineOnLocalClientByExiting", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(__instance, null);
+                }
+            }
+        }
+    }
+}
---
+++ RNGMines/RNGMineBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using RNGMines.Patches;
+
+namespace RNGMines
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class RNGMineBase : BaseUnityPlugin
+    {
+        private const string modGUID = "f0ur3y3s.RNGMine";
+        private const string modName = "RNGMine";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static RNGMineBase Instance;
+        static ManualLogSource logger;
+        internal static ManualLogSource GetLogger()
+        {
+            return logger;
+        }
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modName);
+            string icon = @"
+                                              
+                                              
+                                ██    ██      
+                    ██████      ██  ██        
+                  ██      ██                  
+                ██          ████░░    ████    
+                ██                            
+              ██████            ██  ██        
+              ██████            ██    ██      
+          ██████████████                      
+        ██████░░░░░░░░▓▓██                    
+      ██████░░░░░░░░  ▓▓▓▓██                  
+      ██████▓▓▓▓▓▓▓▓    ▓▓██                  
+    ████████▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓██                
+    ████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██                
+    ████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██                
+      ████████▓▓▓▓▓▓▓▓▓▓▓▓██                  
+      ████████████▓▓▓▓▓▓████                  
+        ██████████████████                    
+          ██████████████                      
+              ██████                          
+";
+            logger.LogWarning(icon);
+            logger.LogInfo(" RNGMine Loaded!");
+            logger.LogInfo("Patching RNGMineBase");
+            harmony.PatchAll(typeof(RNGMineBase));
+            logger.LogInfo("Patching PlayerItemSlots");
+            harmony.PatchAll(typeof(MinePatch));
+        }
+    }
+}
